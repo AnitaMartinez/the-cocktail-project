ca3c6d61ea21272f891515889392232726e68f18
“Ana” <amartinezaguilar6@gmail.com>
2018-02-08 11:45:03 +0100

    refactor function fetchInfoBuses

--- src/App.js
+++ src/App.js
@@ -12,14 +12,13 @@ class App extends Component {
     this.state = {
       stopsBus: {},
     }
-    this.fetchInfoBuses = this.fetchInfoBuses.bind(this);
   }
 
   fetchInfoBuses() {
     const idClient = "WEB.SERV.redlim@gmail.com";
     const passKey = "FB5B0E17-88EB-407E-A222-97F0916E0C41";
     const urlGetStopsFromXY = "https://openbus.emtmadrid.es:9443/emt-proxy-server/last/geo/GetStopsFromXY.php";
-    let that = this
+    const that = this
     fetch(urlGetStopsFromXY, {
       method: "POST",
       headers: {
@@ -27,9 +26,9 @@ class App extends Component {
       },
       body:
       "idClient="+idClient+"&passKey="+passKey+"&latitude="+latitude+"&longitude="+longitude+"&Radius="+radius+"&statistics=&cultureInfo=",
-    }).then(function(response) {
+    }).then((response) => {
         return response.json();
-    }).then(function (data) {
+    }).then((data) => {
         that.setState({
           stopsBus: data,
         });
