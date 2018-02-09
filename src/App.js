36784365d33d249dc61477593546e65c4b523682
Adalaber <mvtercero85@gmail.com>
2018-02-09 13:41:16 +0100

    Add Intro Map component with styles except position search text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import Intro from './components/Intro';
+import './index.css';
 
 //The cocktail
 let latitude = 40.454207;
@@ -38,14 +40,17 @@ class App extends Component {
   render() {
     console.log(this.state.stopsBus);
     return (
-      <div style={{width:'400px', height: '400px'}}>
+      <div className= "home">
+      <Intro/>
+      <div className="map">
+      <p className= "map-text-search introduction-text-search right">Buscar por código postal ></p>
       <GoogleMapReact
       defaultCenter={this.props.center}
       defaultZoom={this.props.zoom}
       bootstrapURLKeys={{key: 'AIzaSyC7n0BhHlxsVU_li9hGJMFIFbYQcFqaggw'}}
       >
       </GoogleMapReact>
-
+      </div>
       </div>
     );
   }
