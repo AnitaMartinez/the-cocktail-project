a73f198ef2fcfeb81076c7ed414a9ca493ea3e88
Rita Cascante <margalida.kaskante@gmail.com>
2018-02-09 19:16:35 +0100

    market change

---
+++ src/components/Marker.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Marker extends Component {
+  render() {
+    return(
+      <div className="marker">
+      <img src="https://placehold.it/40x40"/>
+      </div>
+    )
+  }
+}
+
+export default Marker
