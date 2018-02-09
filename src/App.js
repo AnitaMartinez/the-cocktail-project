4a1c7184f26e9c3431d010b9d2257e1c23fa3a2d
“Ana” <amartinezaguilar6@gmail.com>
2018-02-09 20:53:06 +0100

    Insert section-results and footer in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
-import PageResults from './components/PageResults'
 import Stop from './components/Stop'
 import Marker from './components/Marker'
 
@@ -75,8 +74,72 @@ class App extends Component {
           return <Stop stop={stop} key={index}/>
           })}
         </div>
-        <PageResults/>
+
+        <section className="section-results">
+          <h4 className="m-top-none section-title">Resultados</h4>
+          <div className="box-result flex">
+            <div className="flex">
+              <span className="number-bus">17</span>
+              <div className="box-data-section">
+                <h6 className="m-none data-title">Cibeles</h6>
+                <p className="data-medium">Plaza de Cibeles con Pº Recoletos</p>
+              </div>
+            </div>
+            <div className="flex box-info-icon">
+              <img className="icon-bus" src="images/bus.png" alt="icono bus"/>
+              <p className="m-none">Tiempo de espera: 3 min</p>
+            </div>
+          </div>
+          <div className="box-result flex">
+            <div className="flex">
+              <span className="number-bus">17</span>
+              <div className="box-data-section">
+                <h6 className="m-none data-title">Cibeles</h6>
+                <p className="data-medium">Plaza de Cibeles con Pº Recoletos</p>
+              </div>
+            </div>
+            <div className="flex box-info-icon">
+              <img className="icon-bus" src="images/bus.png" alt="icono bus"/>
+              <p className="m-none">Tiempo de espera: 3 min</p>
+            </div>
+          </div>
+          <div className="box-result flex">
+            <div className="flex">
+              <span className="number-bus">17</span>
+              <div className="box-data-section">
+                <h6 className="m-none data-title">Cibeles</h6>
+                <p className="data-medium">Plaza de Cibeles con Pº Recoletos</p>
+              </div>
+            </div>
+            <div className="flex box-info-icon">
+              <img className="icon-bus" src="images/bus.png" alt="icono bus"/>
+              <p className="m-none">Tiempo de espera: 3 min</p>
+            </div>
+          </div>
+          <div className="box-result flex border-bottom-results">
+            <div className="flex">
+              <span className="number-bus">17</span>
+              <div className="box-data-section">
+                <h6 className="m-none data-title">Cibeles</h6>
+                <p className="data-medium">Plaza de Cibeles con Pº Recoletos</p>
+              </div>
+            </div>
+            <div className="flex box-info-icon">
+              <img className="icon-bus" src="images/bus.png" alt="icono bus"/>
+              <p className="m-none">Tiempo de espera: 3 min</p>
+            </div>
+          </div>
+        </section>
+
+        <div className="box-goUp">
+          <a className="link-goUp" href="#">Volver arriba</a>
+        </div>
+
         </main>
+
+        <footer className="footer footer-font">
+          <p>Hecho por 'Las Apis' de Adalab</p>
+        </footer>
 
       </div>
     );
--- src/components/PageResults.js
+++ src/components/PageResults.js
@@ -1,79 +1,18 @@
-import React, { Component } from 'react';
-
-class PageResults extends Component {
-
-  render() {
-    return (
-      <div>
-      <section className="section-results">
-        <h4 className="m-top-none section-title">Resultados</h4>
-        <div className="box-result flex">
-          <div className="flex">
-            <span className="number-bus">17</span>
-            <div className="box-data-section">
-              <h6 className="m-none data-title">Cibeles</h6>
-              <p className="data-medium">Plaza de Cibeles con Pº Recoletos</p>
-            </div>
-          </div>
-          <div className="flex box-info-icon">
-            <img className="icon-bus" src="images/bus.png" alt="icono bus"/>
-            <p className="m-none">Tiempo de espera: 3 min</p>
-          </div>
-        </div>
-        <div className="box-result flex">
-          <div className="flex">
-            <span className="number-bus">17</span>
-            <div className="box-data-section">
-              <h6 className="m-none data-title">Cibeles</h6>
-              <p className="data-medium">Plaza de Cibeles con Pº Recoletos</p>
-            </div>
-          </div>
-          <div className="flex box-info-icon">
-            <img className="icon-bus" src="images/bus.png" alt="icono bus"/>
-            <p className="m-none">Tiempo de espera: 3 min</p>
-          </div>
-        </div>
-        <div className="box-result flex">
-          <div className="flex">
-            <span className="number-bus">17</span>
-            <div className="box-data-section">
-              <h6 className="m-none data-title">Cibeles</h6>
-              <p className="data-medium">Plaza de Cibeles con Pº Recoletos</p>
-            </div>
-          </div>
-          <div className="flex box-info-icon">
-            <img className="icon-bus" src="images/bus.png" alt="icono bus"/>
-            <p className="m-none">Tiempo de espera: 3 min</p>
-          </div>
-        </div>
-        <div className="box-result flex border-bottom-results">
-          <div className="flex">
-            <span className="number-bus">17</span>
-            <div className="box-data-section">
-              <h6 className="m-none data-title">Cibeles</h6>
-              <p className="data-medium">Plaza de Cibeles con Pº Recoletos</p>
-            </div>
-          </div>
-          <div className="flex box-info-icon">
-            <img className="icon-bus" src="images/bus.png" alt="icono bus"/>
-            <p className="m-none">Tiempo de espera: 3 min</p>
-          </div>
-        </div>
-      </section>
-
-
-      <div className="box-goUp">
-        <a className="link-goUp" href="#">Volver arriba</a>
-      </div>
-
-      <footer className="footer footer-font">
-        <p>Hecho por 'Las Apis' de Adalab</p>
-      </footer>
-
-      </div>
-    );
-  }
-
-}
-
-export default PageResults;
+// import React, { Component } from 'react';
+//
+// class PageResults extends Component {
+//
+//   render() {
+//     return (
+//       <div>
+//
+//
+//
+//
+//       </div>
+//     );
+//   }
+//
+// }
+//
+// export default PageResults;
