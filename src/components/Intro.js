5fcb93b0e6c4e6f02a117d407fab40d6cff2d056
Adalaber <mvtercero85@gmail.com>
2018-02-09 13:49:35 +0100

    Delete font weight in introduction text search typography scss

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,14 +5,14 @@ class Intro extends Component {
   render(){
     return(
 
-    <div className= "intro">
-    <img src="img/marker-icon.svg" className= "marker-icon-intro" alt="icono de marker"/>
-    <h2 className= "home-title introduction-title center">Bienvenido a BusApp</h2>
-    <p className= "home-text introduction-body center">Para comenzar, elige una zona para descubrir las paradas disponibles</p>
-    <button className= "home-button main-button button-light-font" type="button" name="button">Glorieta de bilbao</button>
-    <button className= "home-button main-button button-light-font" type="button" name="button">The cocktail</button>
-    <button className= "home-button main-button button-light-font" type="button" name="button">El campo</button>
-    </div>
+      <div className= "intro">
+      <img src="img/marker-icon.svg" className= "marker-icon-intro" alt="icono de marker"/>
+      <h2 className= "home-title introduction-title center">Bienvenido a BusApp</h2>
+      <p className= "home-text introduction-body center">Para comenzar, elige una zona para descubrir las paradas disponibles</p>
+      <button className= "home-button main-button button-light-font" type="button" name="button">Glorieta de bilbao</button>
+      <button className= "home-button main-button button-light-font" type="button" name="button">The cocktail</button>
+      <button className= "home-button main-button button-light-font" type="button" name="button">El campo</button>
+      </div>
 
     );
   }
