c4c76975c29bfac1b052a693c4973e9094f24743
“Ana” <amartinezaguilar6@gmail.com>
2018-02-14 11:24:20 +0100

    Add class active default

--- src/App.js
+++ src/App.js
@@ -39,21 +39,17 @@ class App extends Component {
   }
 
   handleClickPagination(event) {
-    console.log(document.querySelectorAll(".number-pagination"));
-        this.setState({
-          currentPage: Number(event.target.id)
-        });
-        for(let i = 0; i < document.querySelectorAll(".number-pagination").length; i++) {
-          document.querySelectorAll(".number-pagination")[i].classList.remove('active');
-          event.target.classList.add('active');
-        }
-      }
+    const numberPagination = document.querySelectorAll(".number-pagination");
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+    for(let i = 0; i < numberPagination.length; i++) {
+      numberPagination[i].classList.remove('active');
+      event.target.classList.add('active');
+    }
+  }
 
   fetchInfoBuses(latitude,longitude) {
-    const idClient = "WEB.SERV.redlim@gmail.com";
-    const passKey = "FB5B0E17-88EB-407E-A222-97F0916E0C41";
-    const urlGetStopsFromXY = "https://openbus.emtmadrid.es:9443/emt-proxy-server/last/geo/GetStopsFromXY.php";
-    const that = this
     fetch(urlGetStopsFromXY, {
       method: "POST",
       headers: {
@@ -133,7 +129,7 @@ class App extends Component {
     }
     const renderPageNumbers = pageNumbers.map(number => {
       return (
-          <li className="number-pagination button-light-font" key={number} id={number} onClick={this.handleClickPagination} >
+          <li className={"number-pagination button-light-font " + (number === 1 ? 'active' : "")} key={number} id={number} onClick={this.handleClickPagination} >
             {number}
           </li>
       );
