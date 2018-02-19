4a00feb5d7b59489abd0f5742a94ac9d687a842c
Estela Parrado <estela.parrado@gmail.com>
2018-02-19 16:36:27 +0100

    added transition on show sticky menu

--- src/components/StickyMenu.js
+++ src/components/StickyMenu.js
@@ -18,7 +18,9 @@ componentDidMount(){
 
     render() {
       return(
-        <div id="sticky" className="menu-invisible">
+        <div id="sticky" className="hidden">
+          <h1 className="sticky-title padding-subtitle uppercase no-margin center">bus app</h1>
+          <p className="sticky-subtitle padding-subtitle no-margin center">El mundo, en la palma de tu mano</p>
         </div>
       )
     }
