9d4a44517484ffb2fbb702d38b216b3ddee1e73c
Rita Cascante <margalida.kaskante@gmail.com>
2018-02-21 15:47:14 +0100

    New proposal group page

---
+++ src/components/AllCards.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import Spinner from './Icons/Spinner';
+import Card from './Card';
+import PropTypes from 'prop-types';
+
+
+class AllCards extends Component {
+
+  render(){
+    const stopsBus = this.props.stopsBus;
+    const selectedStop= this.props.selectedStop;
+    const hiddenResults = this.props.hidden ? 'hidden' : '';
+
+    // Pagination
+    const currentPage = this.props.currentPage
+    const elementsPerPage = this.props.elementsPerPage
+    const indexOfLastAll = currentPage * elementsPerPage;
+    const indexOfFirstAll = indexOfLastAll - elementsPerPage;
+    const currentElements = stopsBus.slice(indexOfFirstAll, indexOfLastAll);
+    const renderElementsPage = currentElements.map((stop, index) => {
+      return (
+        <Card
+          stop={stop}
+          key={index}
+          setCurrentStop={this.props.setCurrentStop}
+          selected = {stop === selectedStop}
+        />
+      )
+    });
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(stopsBus.length / elementsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <li key={number}>
+          <button onClick={this.props.onClick} className={"number-pagination button-light-font " + (number === 1 ? 'active' : "")} id={number} type="button" name="button">
+            {number}
+          </button>
+        </li>
+      );
+    });
+
+    return(
+      <section className={`section-cards ${hiddenResults}`}>
+      <h3 className="m-top-none section-title-font section-title">Resultados</h3>
+      {this.props.noResults}
+      { this.props.loading ? null : <Spinner/>  }
+      <div className="container-cards">
+      {renderElementsPage}
+      </div>
+      <ul id="page-numbers flex" className="list-pagination">
+      {renderPageNumbers}
+      </ul>
+      </section>
+    )
+  }
+}
+
+AllCards.propTypes = {
+  stopsBus: PropTypes.array,
+  selectedStop: PropTypes.object,
+  hidden: PropTypes.bool,
+  currentPage: PropTypes.number,
+  elementsPerPage: PropTypes.number,
+  onClick: PropTypes.func,
+  noResults: PropTypes.element,
+  loading: PropTypes.bool
+};
+
+export default AllCards;
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import profilePic from '../images/profile.jpg';
+import profilePic from '../images/team.jpg';
 
 
 
@@ -7,21 +7,33 @@ class Team extends Component {
   render() {
     return(
       <div className="teamWrapper">
+      <div style={{backgroundImage: `url(${profilePic})`}} className="teamImage">
+      <div className="component1">'El minimalismo es mi lema'</div>
+      <div className="component2">'El bid data es mi pasión'</div>
+      <div className="component3">'El bid data es mi pasión'</div>
 
-      <div style={{backgroundImage: `url(${profilePic})`}} className="teamImage">
-       <p className="imgDescription">Ana, programadora front end y amante del Big Data</p>
-      </div>
-      <div style={{backgroundImage: `url(${profilePic})`}} className="teamImage">
-       <p className="imgDescription">Estela, programadora front end y muy minimalista</p>
-      </div>
-      <div style={{backgroundImage: `url(${profilePic})`}} className="teamImage">
-       <p className="imgDescription">María, programadora front end y no desarrolla sin contenido</p>
-      </div>
-      <div style={{backgroundImage: `url(${profilePic})`}} className="teamImage">
-        <p className="imgDescription">Rita, programadora front end y amante de perros salchichas</p>
+
       </div>
 
       </div>
+
+      // <div style={{backgroundImage: `url(${profilePic})`}} className="teamImage">
+      //  <p className="imgDescription">Ana, programadora front end y amante del Big Data</p>
+      // </div>
+      // <div style={{backgroundImage: `url(${profilePic})`}} className="teamImage">
+      //  <p className="imgDescription">Estela, programadora front end y muy minimalista</p>
+      // </div>
+      // <div style={{backgroundImage: `url(${profilePic})`}} className="teamImage">
+      //  <p className="imgDescription">María, programadora front end y no desarrolla sin contenido</p>
+      // </div>
+      // <div style={{backgroundImage: `url(${profilePic})`}} className="teamImage">
+      //   <p className="imgDescription">Rita, programadora front end y amante de perros salchichas</p>
+
+
+
+
+
+
     )
   }
 }
