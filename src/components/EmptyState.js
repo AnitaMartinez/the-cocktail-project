6c4492fa1d1303326d7e9f92456ad585b50550d1
Adalaber <mvtercero85@gmail.com>
2018-02-21 12:31:12 +0100

    Add styles to empty state no results

--- src/components/EmptyState.js
+++ src/components/EmptyState.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react';
 class EmptyState extends Component {
   render(){
     return(
-    <div className= "no-results">
-    <p className= "data-title">No hay resultados</p>
+    <div>
+    <p className= "m-top-none section-text-no-results section-title">No hay resultados</p>
     </div>
     )
   }
