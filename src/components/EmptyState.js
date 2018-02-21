153163f3ad68a03583e8fd815c4fe39044ea0065
“Ana” <amartinezaguilar6@gmail.com>
2018-02-21 16:20:53 +0100

    Refactor sass

--- src/components/EmptyState.js
+++ src/components/EmptyState.js
@@ -4,9 +4,9 @@ import React, { Component } from 'react';
 class EmptyState extends Component {
   render(){
     return(
-    <div>
-    <p className= "m-top-none section-text-no-results section-title">No hay resultados</p>
-    </div>
+      <div>
+        <p className= "m-top-none section-text-no-results section-title">No hay resultados</p>
+      </div>
     )
   }
 }
