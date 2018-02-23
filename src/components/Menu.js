379315c14a697b17d81159327de6bafaf8764e58
“Ana” <amartinezaguilar6@gmail.com>
2018-02-23 21:09:14 +0100

    Remove warning

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import MenuButton from './Icons/MenuButton';
 import CloseButton from './Icons/CloseButton';
